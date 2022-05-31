cf9dbc0661d9a3e066f44a2c6172f45930dd1851
m3xq <z3w1l0q@protonmail.com>
2022-05-31 10:41:26 +0300

    Init

---
+++ spec/telegraphia_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Telegraphia do
+  it "has a version number" do
+    expect(Telegraphia::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
